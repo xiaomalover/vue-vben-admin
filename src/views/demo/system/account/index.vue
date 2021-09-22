d2095af7318187c685756d932974dea1b8112d76
xm <xm@lfcs.com>
2021-09-22 10:50:37 +0800

    Update

--- src/views/demo/system/account/index.vue
+++ src/views/demo/system/account/index.vue
@@ -71,7 +71,7 @@
         showTableSetting: true,
         bordered: true,
         handleSearchInfoFn(info) {
-          console.log('handleSearchInfoFn', info);
+          //console.log('handleSearchInfoFn', info);
           return info;
         },
         actionColumn: {
@@ -89,7 +89,7 @@
       }
 
       function handleEdit(record: Recordable) {
-        console.log(record);
+        //console.log(record);
         openModal(true, {
           record,
           isUpdate: true,
@@ -107,8 +107,8 @@
         if (isUpdate) {
           // 演示不刷新表格直接更新内部数据。
           // 注意：updateTableDataRecord要求表格的rowKey属性为string并且存在于每一行的record的keys中
-          const result = updateTableDataRecord(values.id, values);
-          console.log(result);
+          /*const result = */updateTableDataRecord(values.id, values);
+          //console.log(result);
         } else {
           reload();
         }
